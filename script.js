dc9f073d1b205aaa9d46b3634c68aaeacc16addc
zeneralk28 <133392691+zeneralk28@users.noreply.github.com>
2024-09-27 10:36:59 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// smooth scrolling
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// scroll animation
+document.addEventListener('DOMContentLoaded', function () {
+    const sections = document.querySelectorAll('.home, .info');
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate');
+            } else {
+                entry.target.classList.remove('animate');
+            }
+        });
+    });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
